ccc3cd68c93295c1e460539c9185156e49190d7a
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-27 09:00:23 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////
+/* 
+Q1: Write a program that takes 2 numbers from user and displays the greater number:
+INPUT: 2 8
+OUTPUT: 8 is greater
+*/
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+/*
+Q2: Write a program that takes a number from user, and if the number is negative, make it positive, otherwise print already positive.
+INPUT: -2
+OUTPUT: 2
+Hint:  -2 + (2*2) = 2
+*/
