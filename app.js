26366ef8f8816dd0128cfcb658440c3e5bbf2b3e
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-27 11:25:09 +0500

    Q1: COMPLETE

--- app.js
+++ app.js
@@ -5,13 +5,70 @@ INPUT: 2 8
 OUTPUT: 8 is greater
 */
 ///////////////////////////////////////////////////////////////////////
+var num1 = prompt("Enter-Any-Number From 0 to 1000000");
+var num2 = prompt("Enter-any-number from 0 to 1000000");
+
+num1=parseInt(num1)
+num2=parseInt(num2)
+if(num1>num2){
+    alert(num1 + " Is Greater")
+}
+if(num2>num1){
+    alert(num2 + " Is Greater")
+}
+if(num1==num2){
+    alert("Equal numbers")
+}
 
 
 
-
-/*
+////////////////////////////////////////////////////////////////////////
+/* 
 Q2: Write a program that takes a number from user, and if the number is negative, make it positive, otherwise print already positive.
 INPUT: -2
 OUTPUT: 2
 Hint:  -2 + (2*2) = 2
 */
+///////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+/* 
+ Q3: Write a program that takes a letter from user, and displays if the number is vowel or consonant.
+ Case 1: 
+ INPUT: A
+ OUTPUT: VOWEL
+ Case 2: 
+ INPUT: C
+ OUTPUT: CONSONANT
+*/
+///////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+/* 
+ Q4: Write a program to swap two values.
+Num1 = 2, Num2 = 4
+ Output: Num1 = 4, Num2 = 2
+*/
+///////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+/* 
+ Q5: Write a program that decides if the student is eligible for exam or not by calculating his attendance. If the attendance is below 75%, make him ineligible.
+ Take total classes and classes attend from the user to calculate the percentage.
+ INPUT: Total classes-20, classes attend-10
+ OUTPUT: Ineligible 
+*/
+///////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+/* 
+ Q6: Input 3 sides of triangle from user and display if the triangle is valid or not.
+ Hint: (The sum of all the sides of the triangle should be equal to 180 : side1+side2+side3 = 180)
+ Case 1:
+ INPUT: SIDE1 = 45, SIDE2 = 45, SIDE3 = 90
+ OUTPUT: VALID TRIANGLE
+ Case 2:
+ INPUT: SIDE1 = 40, SIDE2 = 30, SIDE3 = 23
+ OUTPUT: INVALID TRIANGLE
+*/
+///////////////////////////////////////////////////////////////////////
