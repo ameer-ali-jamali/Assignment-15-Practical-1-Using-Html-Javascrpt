6f04e5e3b00797b34b4c0c76f220509468b226ce
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-29 16:25:49 +0500

    uncoment

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ OUTPUT: 8 is greater
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var num1 = prompt("Enter Any Number From 0 to 1000000");
-// var num2 = prompt("Enter-any-number from 0 to 1000000");
-// num1=parseInt(num1)
-// num2=parseInt(num2)
-// if(num1>num2){
-//  alert(num1 + " Is Greater")
-// }
-// if(num2>num1){
-//  alert(num2 + " Is Greater")
-// }
-// if(num1==num2){
-//  alert("Equal numbers")
-// }
+var num1 = prompt("Enter Any Number From 0 to 1000000");
+var num2 = prompt("Enter-any-number from 0 to 1000000");
+num1=parseInt(num1)
+num2=parseInt(num2)
+if(num1>num2){
+ alert(num1 + " Is Greater")
+}
+if(num2>num1){
+ alert(num2 + " Is Greater")
+}
+if(num1==num2){
+ alert("Equal numbers")
+}
 
 ////////////////////////////////////////////////////////////////////////
 /* 
@@ -29,17 +29,17 @@ Hint:  -2 + (2*2) = 2
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var num1 = prompt("Convert Negative Numbers To Positive");
-// num1 = parseInt(num1)
-// if(num1>0){
-//   alert("Alredy Posative")
-// }
-// if(num1<0){
-//   alert(num1%num1-num1)
-//  }
-// if(num1==0){
-//   alert("Alredy Posative")
-// }
+var num1 = prompt("Convert Negative Numbers To Positive");
+num1 = parseInt(num1)
+if(num1>0){
+  alert("Alredy Posative")
+}
+if(num1<0){
+  alert(num1%num1-num1)
+ }
+if(num1==0){
+  alert("Alredy Posative")
+}
 
 ////////////////////////////////////////////////////////////////////////
 /* 
@@ -53,16 +53,16 @@ Hint:  -2 + (2*2) = 2
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var num1 =prompt("Enter Any Leters To Know Vowel letters Or Not")
-// if(num1=="a" || num1=="A" || num1=="E" || num1=="e" || num1=="O" || num1=="o" || num1=="I" || num1=="i"||    num1=="U" || num1=="u" ){
-//  alert("VOWEL")
-// }
-// if(num1>0 || num1<0){
-// alert("Please tpe Text")
-// }
-// else{
-//     alert("CONSONANT")
-// }
+var num1 =prompt("Enter Any Leters To Know Vowel letters Or Not")
+if(num1=="a" || num1=="A" || num1=="E" || num1=="e" || num1=="O" || num1=="o" || num1=="I" || num1=="i"||    num1=="U" || num1=="u" ){
+ alert("VOWEL")
+}
+if(num1>0 || num1<0){
+alert("Please tpe Text")
+}
+else{
+    alert("CONSONANT")
+}
 
 ////////////////////////////////////////////////////////////////////////
 /* 
@@ -72,14 +72,14 @@ Num1 = 2, Num2 = 4
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var num1 = parseInt(prompt("Plese Input Number"))
-// var num2 = parseInt(prompt("Plese Input Number"))
-// if(num2){
-// alert(num1)
-// }
-// if(num1){
-// alert(num2)
-// }
+var num1 = parseInt(prompt("Plese Input Number"))
+var num2 = parseInt(prompt("Plese Input Number"))
+if(num2){
+alert(num1)
+}
+if(num1){
+alert(num2)
+}
 
 ////////////////////////////////////////////////////////////////////////
 /* 
@@ -90,19 +90,19 @@ Num1 = 2, Num2 = 4
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var num1 = prompt("Plese Enter Your Total  Classes In Numbers");
-// var num2 = prompt("Plese Enter Your Attend Classes In Numbers");
-// num1 = parseInt(num1);
-// num2 = parseInt(num2);
-// if(num1&&num2/100&&num2*100/num1<75){
-//     alert("Ineligible");
-// }
-// if(num1&&num2/100&&num2*100/num1>=75 && num1>=num2){
-//     alert("Eligible");
-// }
-// if(num2>=num1){
-//     alert("Please Try Again");
-// }
+var num1 = prompt("Plese Enter Your Total  Classes In Numbers");
+var num2 = prompt("Plese Enter Your Attend Classes In Numbers");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+if(num1&&num2/100&&num2*100/num1<75){
+    alert("Ineligible");
+}
+if(num1&&num2/100&&num2*100/num1>=75 && num1>=num2){
+    alert("Eligible");
+}
+if(num2>=num1){
+    alert("Please Try Again");
+}
 
 ////////////////////////////////////////////////////////////////////////
 /* 
@@ -117,15 +117,15 @@ Num1 = 2, Num2 = 4
 */
 ///////////////////////////////////////////////////////////////////////
 
-// var side1 =  prompt("Please input ");
-// var side2 =  prompt("Please input ");
-// var side3 =  prompt("Please input ");
-// side1 = parseInt(side1);
-// side2 = parseInt(side2);
-// side3 = parseInt(side3);
-// if(side1+side2+side3>=180){
-//   alert("VALID TRIANGLE");
-// }
-// else{
-//   alert("INVALID TRIANGLE");
-// }
+var side1 =  prompt("Please input ");
+var side2 =  prompt("Please input ");
+var side3 =  prompt("Please input ");
+side1 = parseInt(side1);
+side2 = parseInt(side2);
+side3 = parseInt(side3);
+if(side1+side2+side3>=180){
+  alert("VALID TRIANGLE");
+}
+else{
+  alert("INVALID TRIANGLE");
+}
